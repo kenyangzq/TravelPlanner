b6147ba317d1b2398b49c2a7be17ef836a76aaaf
Ken Yang <kenyangzq95@outlook.com>
2026-02-07 10:50:24 -0800

    Initial Commit

---
+++ TravelPlanner/TravelPlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelPlannerApp.swift
+//  TravelPlanner
+//
+//  Created by Ken Yang on 2/7/26.
+//
+
+import SwiftUI
+
+@main
+struct TravelPlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TravelPlannerTests/TravelPlannerTests.swift
@@ -0,0 +1,17 @@
+//
+//  TravelPlannerTests.swift
+//  TravelPlannerTests
+//
+//  Created by Ken Yang on 2/7/26.
+//
+
+import Testing
+@testable import TravelPlanner
+
+struct TravelPlannerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
